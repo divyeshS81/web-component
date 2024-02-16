3767c4b0aa6452d23fe1423d552d9ce1657ed797
divyesh sarvaiya <divyeshsarvaiya999@gmail.com>
2024-02-16 12:48:17 +0530

    project Complate of CHATBOT

---
+++ chatbot for website/static/script/responsis.js
@@ -0,0 +1,25 @@
+function getBotResponse(input) {
+      if (input == "hii") {
+            return "hello";
+      } else if (input == "how are you?"){
+            return "i'm fine, What About You?";
+      } else if (input == "") {
+            return "rock";
+      } else if (input == "how are you?"){
+            return "i'm fine, What About You?";
+      }
+
+
+      if (input == "Hello"){
+            return "Hello There!";
+      } else if (input == "good bye"){
+            return "Talk to you later!";
+
+      }else{
+            return "Try asking something else!";
+      }
+
+
+      if (input == "How Are you?")
+      return "i'm Fine, what about you?";
+}
